9dd4ac228b3e2bb5e783b5c73c8d30d5a72d96cd
joonas.fi <pompo500@xs.fi>
2017-01-11 16:02:38 +0200

    Remove debug echo

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 configure_ssl_cert_private_key() {
 	SSL_CERT_PRIVATE_KEY_DECODED=$(echo -n "$SSL_CERT_PRIVATE_KEY" | base64 -d)
 
-	echo "$SSL_CERT_PRIVATE_KEY_DECODED"
-
 	echo -n "$SSL_CERT_PRIVATE_KEY_DECODED" > /etc/traefik/ssl_certs/public_https.key
 }
 
